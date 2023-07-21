d6e9565f4a3185cb20b6176735baa83841ae8af8
Panthro22 <jordonpaynter22@gmail.com>
2023-07-21 12:59:06 -0600

    Add CTA1 directory and Started BankAccount

---
+++ CTA1/BankAccount.java
@@ -0,0 +1,12 @@
+package CTA1;
+
+public class BankAccount {
+    private String firstName;
+    private String lastName;
+    private int accountID;
+    private double balance;
+
+    public BankAccount() {
+        balance = 0.0;
+    }
+}
