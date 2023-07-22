da2de6cb082c63ff51fc174034737d561cf8528d
Panthro22 <jordonpaynter22@gmail.com>
2023-07-22 00:54:20 -0600

    Added additional lines for testing.

--- CTA1/TestCheckingAccount.java
+++ CTA1/TestCheckingAccount.java
@@ -6,15 +6,21 @@ public class TestCheckingAccount {
         checkingAccount.setFirstName("Jill");
         checkingAccount.setLastName("Larsen");
         checkingAccount.setAccountID(123456789);
-        checkingAccount.setInterestRate(0.05);
-        checkingAccount.deposit(1000.0);
+        checkingAccount.setInterestRate(0.15);
+        
+        checkingAccount.deposit(1500.0);
         checkingAccount.getBalance();
+        
+        checkingAccount.displayAccount();
         
         checkingAccount.processWithdrawal(200.0);
         checkingAccount.getBalance();
-        checkingAccount.processWithdrawal(1500.0);
+        checkingAccount.processWithdrawal(1450.0);
         checkingAccount.getBalance();
-
+        checkingAccount.deposit(2530.0);
+        checkingAccount.getBalance();
+        checkingAccount.processWithdrawal(1450.0);
+        checkingAccount.getBalance();
         
         checkingAccount.displayAccount();
     }
