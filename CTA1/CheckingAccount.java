1335c3ceb5cfa8a39141de653e8a362e8e2917d8
Panthro22 <jordonpaynter22@gmail.com>
2023-07-21 18:53:05 -0600

    Finished TestClass to ensure that BankAccount and CheckingAccount Work Properly. Also provided user with additional Print out statement to show Operations were taking place during operation.

--- CTA1/CheckingAccount.java
+++ CTA1/CheckingAccount.java
@@ -5,15 +5,20 @@ public class CheckingAccount extends BankAccount {
 
     public CheckingAccount() {
         super();
-        this.interestRate = 0.05;
+        this.interestRate = 0.00;
+    }
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
     }
     public void processWithdrawal(double amount) {
         double overdraftFee = 30.0;
 
         if (this.getBalance() >= amount) {
             this.withdrawal(amount);
+            System.out.println("Withdrawal: $" + amount);
         } else {
             this.withdrawal(amount + overdraftFee);
+            System.out.println("Withdrawal: $" + amount);
             System.out.println("Negative balance (including $30 overdraft fee): $" + (this.getBalance()));
             System.out.println("An overdraft fee of $30 has been assessed.");
         }
