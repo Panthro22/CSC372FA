8fab4cd490f9f70633c4d70ed199750d8e5d7ed2
Panthro22 <jordonpaynter22@gmail.com>
2023-07-29 11:40:47 -0600

    Finished building out constructor for BankGUI

--- CTA2/BankGUI.java
+++ CTA2/BankGUI.java
@@ -9,7 +9,11 @@ import java.awt.event.ActionListener;
 
 public class BankGUI extends JFrame implements ActionListener {
     private CheckingAccount checkingAccount;
-    
+    private JLabel balanceLabel;
+    private JButton depositButton;
+    private JButton withdrawButton;
+    private JTextField amountField;
+
     public BankGUI() {
         checkingAccount = new CheckingAccount(); // Create a CheckingAccount instance
         checkingAccount.setAccountID(123456789); // Set the account number
@@ -17,6 +21,33 @@ public class BankGUI extends JFrame implements ActionListener {
         checkingAccount.setLastName("Rowly"); // Set the last name
         checkingAccount.setInterestRate(0.01); // Set the interest rate
         checkingAccount.deposit(1000.00); // Deposit $1000.00
+
+        //Create a GUI Components
+        balanceLabel = new JLabel("Balance: $" + checkingAccount.getBalance());
+        depositButton = new JButton("Deposit");
+        withdrawButton = new JButton("Withdraw");
+        amountField = new JTextField(10);
+        
+        // Add action listeners
+        depositButton.addActionListener(this);
+        withdrawButton.addActionListener(this);
+
+        // Set up the layout
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        panel.add(balanceLabel);
+        panel.add(amountField);
+        panel.add(depositButton);
+        panel.add(withdrawButton);
+
+        // Add the panel to the JFrame
+        getContentPane().add(panel);
+
+        // Set JFrame properties
+        setTitle("All Legit Banking");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null); // Center the window on the screen
     }
 
     @Override
